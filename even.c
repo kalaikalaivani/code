92daded7866fad6adebde2db55a0c3d51eaf2c25
kalaikalaivani <kalaivani1022@gmail.com>
2016-07-06 11:48:50 +0530

    Create even.c

---
+++ even.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n NUMBER IS ODD OR EVEN");
+printf("\n Enter the number:");
+scanf("%d",&n);
+if(n%2==0)
+{
+ printf("\n GIVEN NUMBER IS EVEN");
+ 
+ }
+ else
+ {
+ printf("\n GIVEN NUMBER IS ODD ");
+ }
+ getch();
+ }
