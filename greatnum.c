ab104026a2a2ecec1491bfe569822dd2a3e68251
kalaikalaivani <kalaivani1022@gmail.com>
2016-07-06 16:00:50 +0530

    Create greatnum.c

---
+++ greatnum.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n Enter the number a,b,c:");
+scanf("\n%d\n%d\n%d",&a,&b,&c);
+if(a>b&&a>c)
+ printf("\n Ais a greatest number");
+ifelse(b>a&&b>c)
+ printf("\n Bis a greatest number");
+ else
+ printf("\n Cis a greatest number");
+ getch();
+ }
