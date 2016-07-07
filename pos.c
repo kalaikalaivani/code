e3190f1456fbbd3ff06fefad3960437b2765d86a
kalaikalaivani <kalaivani1022@gmail.com>
2016-07-07 08:30:53 +0530

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("\n NUMBER IS POSITIVE OR NOT");
+printf("\n Enter the number:");
+scanf("%d",&n);
+if(n>0)
+{
+ printf("\n GIVEN NUMBER IS POSITIVE ");
+ 
+ }
+ ifelse(i==0)
+ {
+ printf("GIVEN NUMBER IS ZERO");
+ }
+ else
+ {
+ printf("\n GIVEN NUMBER IS NEGATIVE");
+ }
+ getch();
+ }
+ 
