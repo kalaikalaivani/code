f190dff997adb8e2bf0500ae2119c67ec277963b
kalaikalaivani <kalaivani1022@gmail.com>
2016-07-07 08:38:43 +0530

    Update and rename genfib.c to fibgen.c

--- fibgen.c
+++ fibgen.c
@@ -4,14 +4,14 @@ void main()
 {
 int a=0,b=1,D;
 clrscr();
-printf("\n GENERATION OF FIBONACCI SERIES");
+printf("\n GENERATION OF FIBONACCI SERIES ");
 printf("%d\n%d",a,b);
 for(i=2;i<=10;i++)
 {
 c=a+b;
 printf("\n %d",D);
 a=b;
-b=c;
+b=D;
 }
 getch();
 }
